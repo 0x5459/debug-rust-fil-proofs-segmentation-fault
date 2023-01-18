e876a2c2b5fb7ed8fbb729a12c47cef7c06731b3
0x5459 <0x5459@protonmail.com>
2023-01-18 11:40:03 +0800

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
+use std::path::PathBuf;
+
 use filecoin_proofs_api::{
-    post::generate_winning_post_with_vanilla, ProverId, RegisteredPoStProof,
+    post::generate_single_vanilla_proof, PrivateReplicaInfo, RegisteredPoStProof,
 };
-use fvm_shared::address::Address;
 
 fn main() {
-    let vanilla_proof = include_bytes!("../vanilla.dat").to_vec();
-    let miner_id = 25873u64;
-    let mut randomness = [0u8; 32];
-    randomness[31] &= 0x3f;
+    let comm_r = [
+        116, 223, 199, 29, 80, 110, 112, 120, 25, 82, 101, 133, 64, 145, 146, 193, 2, 117, 124, 22,
+        202, 37, 87, 235, 199, 215, 65, 84, 34, 42, 157, 8,
+    ];
+    let registered_proof = RegisteredPoStProof::StackedDrgWindow32GiBV1;
+    let cache_dir_path =
+        PathBuf::from("/mnt/10.0.6.36/disk1/lotusminer_1005/cache/s-t024972-163653");
+    let replica_path =
+        PathBuf::from("/mnt/10.0.6.36/disk1/lotusminer_1005/sealed/s-t024972-163653");
+    let challenges = vec![652308141];
 
-    // let prover_id: ProverId = Address::new_id(miner_id).to_bytes().try_into().unwrap();
-    let vanilla_proofs = vec![vanilla_proof];
-    let res = generate_winning_post_with_vanilla(
-        RegisteredPoStProof::StackedDrgWinning32GiBV1,
-        &randomness,
-        [0; 32],
-        &vanilla_proofs,
-    )
-    .unwrap();
+    let rep = PrivateReplicaInfo::new(
+        registered_proof.into(),
+        comm_r,
+        cache_dir_path,
+        replica_path,
+    );
+
+    let res = generate_single_vanilla_proof(registered_proof, 163653.into(), &rep, &challenges);
+
     println!("{:?}", res);
 }
