138480880d8c4958d1e62806fef95ab6983e638f
0x5459 <0x5459@protonmail.com>
2023-01-18 15:41:54 +0800

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,46 @@
-use std::path::PathBuf;
-
-use filecoin_proofs_api::{
-    post::generate_single_vanilla_proof, PrivateReplicaInfo, RegisteredPoStProof,
+use std::{
+    fs::{self, File},
+    io::{self, BufRead},
+    path::PathBuf,
 };
 
-fn main() {
-    let comm_r = [
-        116, 223, 199, 29, 80, 110, 112, 120, 25, 82, 101, 133, 64, 145, 146, 193, 2, 117, 124, 22,
-        202, 37, 87, 235, 199, 215, 65, 84, 34, 42, 157, 8,
-    ];
+use anyhow::Result;
+use filecoin_proofs_api::{
+    post::generate_single_vanilla_proof, Commitment, PrivateReplicaInfo, RegisteredPoStProof,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct X {
+    comm_r: Commitment,
+    cache_dir_path: PathBuf,
+    replica_path: PathBuf,
+    challenges: Vec<u64>,
+    sector_id: u64,
+}
+
+fn main() -> Result<()> {
     let registered_proof = RegisteredPoStProof::StackedDrgWindow32GiBV1;
-    let cache_dir_path =
-        PathBuf::from("/mnt/10.0.6.36/disk1/lotusminer_1005/cache/s-t024972-163653");
-    let replica_path =
-        PathBuf::from("/mnt/10.0.6.36/disk1/lotusminer_1005/sealed/s-t024972-163653");
-    let challenges = vec![652308141];
 
-    let rep = PrivateReplicaInfo::new(
-        registered_proof.into(),
-        comm_r,
-        cache_dir_path,
-        replica_path,
-    );
+    let f = File::open(PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("1.txt"))?;
 
-    let res = generate_single_vanilla_proof(registered_proof, 163653.into(), &rep, &challenges);
+    let lines = io::BufReader::new(f).lines();
 
-    println!("{:?}", res);
+    for line in lines {
+        let x: X = serde_json::from_str(line.unwrap().as_str())?;
+
+        let rep =
+            PrivateReplicaInfo::new(registered_proof, x.comm_r, x.cache_dir_path, x.replica_path);
+
+        let res = generate_single_vanilla_proof(
+            registered_proof,
+            x.sector_id.into(),
+            &rep,
+            &x.challenges,
+        );
+
+        println!("{:?}", res);
+    }
+
+    Ok(())
 }
